4d2eec79a60eced7c809d094d0752add7a6bce28
stacknow-devops <stacknow.devops@gmail.com>
2025-08-06 10:09:45 -0400

    deploy via webhook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and lets test redeploy.
+          Edit <code>src/App.js</code> and lets test webhook.
         </p>
         <a
           className="App-link"

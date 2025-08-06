d018020adba88bf5b0ad1aaca1e6c00c0701096f
stacknow-devops <stacknow.devops@gmail.com>
2025-08-06 10:11:14 -0400

    webhook test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and lets test webhook.
+          Edit <code>src/App.js</code> and lets test webhook now.
         </p>
         <a
           className="App-link"
